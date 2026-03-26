79b05083e8614754273138b613f50ca95c65dafb
RQchen095 <chenruoqi950@gmail.com>
2026-03-26 11:34:43 +0800

    v0

---
+++ index.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    link = "<h1>歡迎進入陳若綺的網站</h1>"
+    link += "<a href=/mis>課程</a><hr>"
+    link += "<a href=/today>現在日期時間</a><hr>"
+    link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome>歡迎</a><hr>"
+    link += "<a href=/account>密碼</a><hr>"    
+    return link
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime=str(now))
+
+@app.route("/me")
+def me():
+    return render_template("mis2026b.html")
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
